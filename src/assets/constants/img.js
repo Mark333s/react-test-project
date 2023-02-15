68bf6c43b3d4f070c85455381f280dbcb9eecb92
Marko Melnychyn <markomelnychyn333@gmail.com>
2023-02-15 12:18:45 +0200

    Updates

--- src/assets/constants/img.js
+++
@@ -1,7 +0,0 @@
-import Avatar from "../img/UserAvatar.svg";
-import Star from "../img/Star.svg";
-import UserIcon from "../img/UserIcon.svg";
-
-export const avatar = Avatar;
-export const star = Star;
-export const userIcon = UserIcon;
--- src/redux/store.js
+++
@@ -1,6 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import repositories from "./slices/repoSlice";
-
-export const store = configureStore({
-  reducer: { repositories },
-});
